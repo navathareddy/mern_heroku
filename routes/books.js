2c632af1d299fe1071e82166a2b33ab21ffc77c8
navathareddy <navatha.reddy1@gmail.com>
2020-08-20 22:10:16 -0400

    m

--- routes/books.js
+++ routes/books.js
@@ -3,9 +3,7 @@ const book_router = express.Router();
 const path = require('path');
 const Book = require("../models/books");
 
-book_router.use(function(req, res) {
-	res.sendFile(path.join(__dirname, '../client/build/index.html'));
-});
+
 
 book_router.get("/", async (req, res) => {
   const books = await Book.find();
