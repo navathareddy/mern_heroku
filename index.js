14f4ecee78c34d70197296500537df94cd499e8b
navathareddy <navatha.reddy1@gmail.com>
2020-08-20 22:32:45 -0400

    commit all changes

--- index.js
+++ index.js
@@ -1,13 +1,14 @@
 const express= require('express')
 const app=express()
 const mongoose=require('mongoose')
+const path = require('path');
 const routes=require('./routes/books')
 const URI = require('./config/index');
 
 app.use(express.json())
 
-app.use(express.static('client/build'));
-app.use('/api/books',routes)
+
+
 
 mongoose
   .connect(process.env.MONGODB_URI || URI, {
@@ -16,7 +17,15 @@ mongoose
   })
   .then(() => console.log("connected successfully"))
   .catch((err) => console.log(err));
+
+
 app.use(express.json());
+
+app.use('/api/books',routes)
+app.use(express.static('client/build'));
+app.get('*',(req,res)=>{
+  res.sendFile(path.resolve(__dirname, '../client/build/index.html'))
+})
 
 const PORT=process.env.PORT || 5000
 
